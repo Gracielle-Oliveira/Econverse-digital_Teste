19ababd7917eb7c80bbb69381f946541b47bb8e7
Gracielle Oliveira <gra2gso@gmail.com>
2021-02-10 23:32:23 -0300

    Avanço da página com incrementação do CSS

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CarouselComponent } from './carousel/carousel.component';
+import { ShowcaseComponent } from './showcase/showcase.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CarouselComponent,
+    ShowcaseComponent
   ],
   imports: [
     BrowserModule,
